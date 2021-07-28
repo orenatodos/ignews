c685b74a44cf81d1db43d8278e1f9e3aaf848786
Renato Silva <orenatodos@gmail.com>
2021-07-28 09:10:02 -0300

    :lipstick: feat: create global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyles from 'styles/global'
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  )
 }
 
 export default MyApp
---
+++ src/styles/global.ts
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  :root {
+    --white: #fff;
+
+    --gray-100: #e1e1e6;
+    --gray-300: #a8a8b3;
+    --gray-900: #121214;
+
+    --cyan-500: #61dafb;
+    --yellow-500: #eba417;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  body {
+    background: var(--gray-900);
+    color: var(--white);
+  }
+
+  body,
+  button,
+  input,
+  textarea,
+  select {
+    font: 400 1.6rem -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+    height: auto;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
