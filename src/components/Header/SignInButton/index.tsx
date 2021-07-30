d827b1bee73445b5a0b8e4aa5ed8fdd057ff7dd3
Renato Silva <orenatodos@gmail.com>
2021-07-30 10:18:47 -0300

    :sparkles: feat: add auth with GitHub Provider

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,16 +1,21 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import * as S from './styles'
 
 const SignInButton = () => {
-  const isLogged = true
+  const [session] = useSession()
 
   return (
-    <S.Wrapper type="button" isLogged={isLogged}>
+    <S.Wrapper
+      type="button"
+      isLogged={!!session}
+      onClick={() => session ? signOut() : signIn('github')}
+    >
       <FaGithub size={20} />
-      { isLogged ? 'Renato Silva' : 'Sign in with GitHub' }
-      { isLogged && <FiX color="#737380" /> }
+      { session ? session.user?.name : 'Sign in with GitHub' }
+      { session && <FiX color="#737380" /> }
     </S.Wrapper>
   )
 }
