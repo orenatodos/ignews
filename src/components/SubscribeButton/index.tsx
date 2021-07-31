03168dfce9e08b6cbaf70a3f9af1828931df66ab
Renato Silva <orenatodos@gmail.com>
2021-07-31 09:43:58 -0300

    :sparkles: feat: redirect user to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,9 +1,36 @@
-import * as S from './styles'
+import { useCallback } from 'react'
+import { signIn, useSession } from 'next-auth/client'
 
-const SubscribeButton = () => (
-  <S.Wrapper type="button">
-    Subscribe now
-  </S.Wrapper>
-)
+import * as S from './styles'
+import api from 'services/api'
+import getStripeJs from 'services/stripe-js'
+
+const SubscribeButton = () => {
+  const [session] = useSession()
+
+  const handleSubscribe = useCallback(async () => {
+    if (session) {
+      signIn('github')
+    }
+
+    try {
+      const response = await api.post('subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }, [])
+
+  return (
+    <S.Wrapper type="button" onClick={handleSubscribe}>
+      Subscribe now
+    </S.Wrapper>
+  )
+}
 
 export default SubscribeButton
